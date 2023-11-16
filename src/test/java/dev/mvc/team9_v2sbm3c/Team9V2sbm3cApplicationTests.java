c5b36290772f1da642319522c8bfc4ebb71c1af2
blenbre777 <blenbre777@naver.com>
2023-11-16 10:15:40 +0900

    test : plan read_all() test 코드

--- src/test/java/dev/mvc/team9_v2sbm3c/Team9V2sbm3cApplicationTests.java
+++ src/test/java/dev/mvc/team9_v2sbm3c/Team9V2sbm3cApplicationTests.java
@@ -1,5 +1,7 @@
 package dev.mvc.team9_v2sbm3c;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,10 +17,19 @@ class Team9V2sbm3cApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	/*
+	//planID를 통한 조회
 	@Test
 	public void testRead() {
 		PlanVO planVO = this.planProc.read(1);
 		System.out.println(planVO.toString());
 		
+	}
+	*/
+	@Test
+	public void testRead_all() {
+		ArrayList<PlanVO> list = this.planProc.list_all();
+		System.out.println(list);
 	}
 }
