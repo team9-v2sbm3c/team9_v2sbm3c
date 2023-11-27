3413843044e7a64cdd2ad611b69995659e5493b9
blenbre777 <blenbre777@naver.com>
2023-11-27 19:25:02 +0900

    feat : erd 기반 sql 작성2

--- src/main/webapp/WEB-INF/doc/activity.sql
+++ src/main/webapp/WEB-INF/doc/activity.sql
@@ -1,6 +1,9 @@
 /**********************************/
 /* Table Name: 활동 */
 /**********************************/
+
+DROP TABLE activity CASCADE CONSTRAINTS;
+
 CREATE TABLE activity(
 		activityID                    		NUMBER(10)		 NOT NULL,
 		planID                        		NUMBER(10)		 NULL ,
@@ -12,15 +15,32 @@ COMMENT ON COLUMN activity.activityID is '활동 번호';
 COMMENT ON COLUMN activity.planID is '여행지 번호';
 COMMENT ON COLUMN activity.aname is '활동 이름';
 
-
 ALTER TABLE activity ADD CONSTRAINT IDX_activity_PK PRIMARY KEY (activityID);
 ALTER TABLE activity ADD CONSTRAINT IDX_activity_FK0 FOREIGN KEY (planID) REFERENCES plan (planID);
 
+DROP SEQUENCE activity_seq;
+
+CREATE SEQUENCE activity_seq
+  START WITH 1         -- 시작 번호
+  INCREMENT BY 1       -- 증가값
+  MAXVALUE 9999999999  -- 최대값: 9999999999 --> NUMBER(10) 대응
+  CACHE 2              -- 2번은 메모리에서만 계산
+  NOCYCLE;             -- 다시 1부터 생성되는 것을 방지
 
 --Create
+INSERT INTO activity(activityID, planID,aname) VALUES(activity_seq.nextval,1,'문화체험'); 
+INSERT INTO activity(activityID, planID,aname) VALUES(activity_seq.nextval,2,'스포츠'); 
+INSERT INTO activity(activityID, planID,aname) VALUES(activity_seq.nextval,3,'문화체험'); 
 
 --Read
+SELECT activityID, planID, aname FROM activity WHERE activityID=1;
+
+SELECT activityID, planID, aname FROM activity WHERE aname='스포츠';
+
+SELECT * FROM activity;
 
 --Update
+UPDATE activity SET aname='스포츠' WHERE planID=1;
 
 --Delete
+DELETE FROM activity WHERE planID=1;
--- src/main/webapp/WEB-INF/doc/schedule.sql
+++ src/main/webapp/WEB-INF/doc/schedule.sql
@@ -20,10 +20,36 @@ COMMENT ON COLUMN schedule.contentsID is '여행컨텐츠 번호';
 ALTER TABLE schedule ADD CONSTRAINT IDX_schedule_PK PRIMARY KEY (scheduleID);
 ALTER TABLE schedule ADD CONSTRAINT IDX_schedule_FK0 FOREIGN KEY (contentsID) REFERENCES contents (contentsID);
 
+DROP SEQUENCE schedule_seq;
+
+CREATE SEQUENCE schedule_seq
+  START WITH 1         -- 시작 번호
+  INCREMENT BY 1       -- 증가값
+  MAXVALUE 9999999999  -- 최대값: 9999999999 --> NUMBER(10) 대응
+  CACHE 2              -- 2번은 메모리에서만 계산
+  NOCYCLE;             -- 다시 1부터 생성되는 것을 방지
+
 --Create
+INSERT INTO schedule(scheduleID, sdescription, date, sname,contentsID) VALUES(schedule_seq.nextval, '서울 야간 스키',sysdate,'서울 스키',1); 
 
 --Read
+SELECT scheduleID, pname FROM plan WHERE planID=1;
+
+SELECT * FROM schedule;
 
 --Update
+1)스케줄 설명 변경
+UPDATE schedule SET sdescription='' WHERE scheduleID=1;
+
+2)스케줄 명 변경 
+UPDATE schedule SET sname='' WHERE scheduleID=1;
+
+3)스케줄 시간 변경
+UPDATE schedule SET date='' WHERE scheduleID=1;
 
 --Delete
+1)스케줄 1개 삭제
+DELETE FROM schedule WHERE scheduleID=1;
+
+2)스케줄 전체 삭제
+DELETE FROM schedule WHERE contentsID=1;
