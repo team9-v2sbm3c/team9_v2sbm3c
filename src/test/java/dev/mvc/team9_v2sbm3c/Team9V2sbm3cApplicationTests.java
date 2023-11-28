0b97c02bae9e9691fba6c5bb1586462253837ff9
blenbre777 <blenbre777@naver.com>
2023-11-28 12:33:39 +0900

    test : list_all_plan() 테스트

--- src/test/java/dev/mvc/team9_v2sbm3c/Team9V2sbm3cApplicationTests.java
+++ src/test/java/dev/mvc/team9_v2sbm3c/Team9V2sbm3cApplicationTests.java
@@ -4,12 +4,27 @@ import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import dev.mvc.plan.PlanDAOInter;
+import dev.mvc.plan.PlanVO;
 
 @SpringBootTest
 class Team9V2sbm3cApplicationTests {
+	@Autowired
+	private PlanDAOInter planProc;
+
+	@Qualifier
+	
 	
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testRead_all() {
+		ArrayList<PlanVO> list = this.planProc.list_all_plan();
+		System.out.println(list);
 	}
 }
